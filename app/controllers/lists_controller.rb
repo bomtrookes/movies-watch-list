f6221b99b5c72866b519c4cc447a9fc70ca72155
Tom Brookes <tom.brookes15@gmail.com>
2023-01-26 17:25:01 +0000

    added more controller actions, updated views

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -5,14 +5,32 @@ class ListsController < ApplicationController
     @lists = List.all
   end
 
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entitiy
+    end
+  end
+
   def show
-    @bookmarks = Bookmark.all
+    @lists = List.all
+    @bookmark = Bookmark.new
   end
 
   private
 
   def set_list
     @list = List.find(params[:id])
+  end
+
+  def list_params
+    params.require(:list).permit(:name)
   end
 
 end
