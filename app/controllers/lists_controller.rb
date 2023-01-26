a767976e8c58d07218027d119e241206d4d0aab8
Tom Brookes <tom.brookes15@gmail.com>
2023-01-26 17:00:17 +0000

    updated lists_controller

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,2 +1,18 @@
 class ListsController < ApplicationController
+  before_action :set_list, only: [:show]
+
+  def index
+    @lists = List.all
+  end
+
+  def show
+    @bookmarks = Bookmark.all
+  end
+
+  private
+
+  def set_list
+    @list = List.find(params[:id])
+  end
+
 end
